13774087168521385382465bedade07ca3308838
Tumma Sri Harsha <tummasriharsha123@gmail.com>
2025-12-10 17:50:37 +0530

    origin error fixed

--- backend/App.js
+++ backend/App.js
@@ -10,11 +10,28 @@ const PORT = process.env.PORT;
 
 
 
-app.use(cors({
-    origin: 'https://spendx.vercel.app',  // Allow all origins
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://spendx.vercel.app"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "DELETE"],
     credentials: true,
-}));
+  })
+);
+
 
 app.use(express.json())
 
